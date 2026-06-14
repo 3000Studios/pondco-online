282761646fabc73d2969e693f87d158b10c1b505
Referrals Dev <dev@referrals.live>
2026-06-14 10:54:05 -0400

    Initialize and wire Firebase SDK for custom telemetry tracking

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2VCBOszos800f-m8inp7XhJkLH4KFkKs",
+  authDomain: "gen-lang-client-0914367944.firebaseapp.com",
+  projectId: "gen-lang-client-0914367944",
+  storageBucket: "gen-lang-client-0914367944.firebasestorage.app",
+  messagingSenderId: "76706851259",
+  appId: "1:76706851259:web:a6fc66937c5098e9d93c9e",
+  measurementId: "G-06L6FY2F3F"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export { app, analytics };
